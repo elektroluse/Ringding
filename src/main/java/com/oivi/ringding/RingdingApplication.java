3519a9ea3ebbfbc797c0e9130ca67f0ba009c2e8
elektroluse <pinkbean@hotmail.no>
2024-10-18 17:22:36 +0200

    project init

---
+++ src/main/java/com/oivi/ringding/RingdingApplication.java
@@ -0,0 +1,13 @@
+package com.oivi.ringding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingdingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingdingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oivi/ringding/RingdingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oivi.ringding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingdingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
